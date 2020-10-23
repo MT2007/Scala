643845f9b9e545ab34b79319414a740f8a744f8c
MT2007 <53901460+MT2007@users.noreply.github.com>
2020-10-23 09:59:28 +0200

    Update Assignment_07_absolute_value.scala

--- Assignment_01/Assignment_07_absolute_value.scala
+++ Assignment_01/Assignment_07_absolute_value.scala
@@ -1,17 +1,23 @@
 package Demo
 
 object absolute_value {
-  def absin (a: Int): Int={
-    if (a < 0)  a * -1
-    else a
-  }
+//  def absin (a: Int): Int={
+//    if (a < 0)  a * -1
+//    else a
+//  }
+
   def abs(a: List[Int]): List[Int]= {
-    var b = a.map(x => absin(x))
+    var b = a.map(x => if (x < 0) x * -1 else x)
     return b
+  }
+
+  def myabs (a: List[Int]): List[Int]= {
+    return a.map(_.abs)
   }
 
   def main(args: Array[String]): Unit = {
     var lst = List(12,8,3,2,6,-5,0,-3,10,-1)
     println(abs(lst))
+    println(myabs(lst))
   }
 }
